01042f4afb04ec2e4e8f472cf5c799c9d66e6bd4
Zoom ThanabutJ <thanabut.j@gmail.com>
2021-08-12 03:53:19 +0700

    change dir name

---
+++ stringsvc1/poxying.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/go-kit/kit/circuitbreaker"
+	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/ratelimit"
+	"github.com/go-kit/kit/sd"
+	"github.com/go-kit/kit/sd/lb"
+	httptransport "github.com/go-kit/kit/transport/http"
+	"github.com/sony/gobreaker"
+	"golang.org/x/time/rate"
+)
+
+func proxyingMidleware(ctx context.Context, instances string, logger log.Logger) ServiceMiddleware {
+	//  If instances is empty, don't poxy.
+	if instances == "" {
+		logger.Log("proxy_to", "none")
+		return func(next StringService) StringService { return next }
+	}
+
+	// Set some parameters for our client.
+	var (
+		qps         = 100
+		maxAttempts = 3
+		maxTime     = 250 * time.Millisecond
+	)
+
+	var (
+		instanceList = split(instances)
+		endpointer   sd.FixedEndpointer
+	)
+	logger.Log("proxy_to", fmt.Sprint(instanceList))
+	for _, instance := range instanceList {
+		var e endpoint.Endpoint
+		e = makeUppercasePoxy(ctx, instance)
+		e = circuitbreaker.Gobreaker(gobreaker.NewCircuitBreaker(gobreaker.Settings{}))(e)
+		e = ratelimit.NewErroringLimiter(rate.NewLimiter(rate.Every(time.Second), gps))(e)
+		endpointer = append(endpointer, e)
+	}
+
+	balancer := lb.NewRoundRobin(endpointer)
+	retry := lb.Retry(maxAttempts, maxTime, balancer)
+
+	return func(next StringService) StringService {
+		return proxymw(ctx, next, retry)
+
+	}
+}
+
+type proxymw struct {
+	ctx       context.Context
+	next      stringService
+	uppercase endpoint.Endpoint
+}
+
+func (mw proxymw) Uppercase(s string) (string, error) {
+	response, err := mw.uppercase(mw.ctx, uppercaseRequest{S: s})
+	if err != nil {
+		return "", err
+	}
+
+	resp := response.(uppercaseResponse)
+	if resp.Err != "" {
+		return resp.V, errors.New(resp.Err)
+	}
+	return resp.V, nil
+}
+
+func makeUppercasePoxy(ctx context.Context, instance string) endpoint.Endpoint {
+	if !strings.HasPrefix(instance, "http") {
+		instance = "http://" + instance
+	}
+	u, err := url.Parse(instance)
+	if err != nil {
+		panic(err)
+	}
+	if u.Path == "" {
+		u.Path = "/uppercase"
+	}
+	return httptransport.NewClient(
+		"GET",
+		u,
+		encodeRequest,
+		decodeUppercaseResponse,
+	).Endpoint()
+}
+
+func split(s string) []string {
+	a := strings.Split(s, ",")
+	for i := range a {
+		a[i] = strings.TrimSpace(a[i])
+	}
+	return a
+}
